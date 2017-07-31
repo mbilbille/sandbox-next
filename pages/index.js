54b9769f7dd7bc8225d3e5b983c5bc6ab736d29a
Matthieu Bilbille <matthieu.bilbille@gmail.com>
2017-07-31 21:22:43 +0200

    Using shared components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+// This is the Link API
+import Link from 'next/link'
+import Layout from '../components/MyLayout.js'
+
 const Index = () => (
-  <div>
-    <p>Hello Next.js
-  </div>
+    <Layout>
+      <p>Hello Next.js</p>
+      <Link>
+        <button>Click here!</button>
+      </Link>
+    </Layout>
 )
 
 export default Index
